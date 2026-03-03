c4e43f29876b2f744cf58476d34cd0a42b68e2da
Klim Sanich <klimfokin05@gmail.com>
2026-03-03 22:00:39 +0700

    Добавлен счетчик пузырьков

--- Bubbles.cpp
+++ Bubbles.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
+#include <cstdio>
 
 using namespace std;
 
@@ -13,6 +14,9 @@ const int POT_LEFT = 100;
 const int POT_RIGHT = WIDTH - 100;
 const int POT_BOTTOM = 60;
 const int POT_TOP = HEIGHT;
+
+int bubbleCounter = 0;
+char counterText[50];
 
 struct Bubble {
     float x, y;
@@ -45,27 +49,34 @@ void drawBubble(float x, float y, float radius) {
     glEnd();
 }
 
+void drawText(float x, float y, const char* text) {
+    glRasterPos2f(x, y);
+    for (const char* c = text; *c != '\0'; c++) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
+    }
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     glColor3f(0.5f, 0.5f, 0.5f);
-
     glRectf(POT_LEFT, POT_BOTTOM, POT_RIGHT, 0);
-
     glRectf(POT_LEFT - 20, POT_TOP, POT_LEFT, 0);
-
     glRectf(POT_RIGHT, POT_TOP, POT_RIGHT + 20, 0);
 
     glColor3f(0.0f, 0.3f, 0.8f);
     glRectf(POT_LEFT, POT_TOP, POT_RIGHT, POT_BOTTOM);
 
     glColor3f(1.0f, 1.0f, 1.0f);
-
     for (int i = 0; i < MAX_BUBBLES; i++) {
         if (bubbles[i].active) {
             drawBubble(bubbles[i].x, bubbles[i].y, bubbles[i].size);
         }
     }
+
+    glColor3f(1.0f, 1.0f, 0.0f);
+    sprintf(counterText, "Bubbles: %d", bubbleCounter);
+    drawText(20, HEIGHT - 30, counterText);
 
     glutSwapBuffers();
 }
@@ -78,6 +89,7 @@ void update(int value) {
                 bubbles[i].y = POT_BOTTOM + 5;
                 bubbles[i].size = 3.0f;
                 bubbles[i].active = true;
+                bubbleCounter++;
                 break;
             }
         }
@@ -86,20 +98,21 @@ void update(int value) {
     for (int i = 0; i < MAX_BUBBLES; i++) {
         if (bubbles[i].active) {
             bubbles[i].y += 2.0f;
-
             bubbles[i].size += 0.15f;
+
 
             if (bubbles[i].y + bubbles[i].size >= POT_TOP - 5) {
                 bubbles[i].active = false;
+                bubbleCounter--;
             }
-
-            if (bubbles[i].size > 25.0f) {
+            else if (bubbles[i].size > 25.0f) {
                 bubbles[i].active = false;
+                bubbleCounter--;
             }
-
-            if (bubbles[i].x - bubbles[i].size < POT_LEFT ||
-                bubbles[i].x + bubbles[i].size > POT_RIGHT) {
+            else if (bubbles[i].x - bubbles[i].size < POT_LEFT ||
+                     bubbles[i].x + bubbles[i].size > POT_RIGHT) {
                 bubbles[i].active = false;
+                bubbleCounter--;
             }
         }
     }
@@ -112,7 +125,7 @@ int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(WIDTH, HEIGHT);
-    glutCreateWindow("������� ���");
+    glutCreateWindow("Bubbles");
 
     init();
 
