e84affd7b9c05b94b2c719c8b7b45607fb3284c1
tu7fly <andrey.ekkert2008@mail.ru>
2026-03-03 21:13:04 +0700

    first commit

---
+++ MultiApp.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+//edit frow Andrew
+///maksim
+int main()
+{
+    int choice;
+
+    while (true)
+    {
+        cout << "\n=== MENU ===\n";
+        cout << "1 - Aquarium\n";
+        cout << "2 - Bubbles\n";
+        cout << "3 - Snow\n";
+        cout << "0 - Exit\n";
+        cout << "Choose:   ";
+
+        cin >> choice;
+        //Нинтендо свичч
+        switch (choice)
+        {
+        case 1:
+            system("Aquarium.exe");
+            break;
+
+        case 2:
+            system("Bubbles.exe");
+            break;
+
+        case 3:
+            system("Snow.exe");
+            break;
+
+        case 0:
+            return 0;
+
+        default:
+            cout << "Wrong choice!\n";
+        }
+    }
+}
